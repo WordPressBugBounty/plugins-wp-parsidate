1943b794265d7df39dc414491bd738501c131c96
Sean Murphy <sean@iamseanmurphy.com>
2024-11-05 21:25:16 +0000

    5.1.0

--- wp-parsidate/includes/admin/gutenberg-jalali-calendar.php
+++ wp-parsidate/includes/admin/gutenberg-jalali-calendar.php
@@ -29,8 +29,7 @@ defined( 'ABSPATH' ) or exit( 'No direct script access allowed' );
  */
 if ( ! function_exists( 'wpp_gutenberg_jalali_calendar_editor_assets' ) ) {
 	function wpp_gutenberg_jalali_calendar_editor_assets() {
-		wp_enqueue_script(
-			'wpp_gutenberg_jalali_calendar_editor_scripts',
+		wp_enqueue_script( 'wpp_gutenberg_jalali_calendar_editor_scripts',
 			WP_PARSI_URL . 'assets/js/gutenberg-jalali-calendar.build.js',
 			array(
 				'wp-plugins',
@@ -46,10 +45,12 @@ if ( ! function_exists( 'wpp_gutenberg_jalali_calendar_editor_assets' ) ) {
 			true
 		);
 
+		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) || wpp_is_active( 'dev_mode' ) ? '' : '.min';
+
 		// Styles.
 		wp_enqueue_style(
 			'wpp_gutenberg_jalali_calendar_editor_styles',
-			WP_PARSI_URL . 'assets/css/gutenberg-jalali-calendar.build.css',
+			WP_PARSI_URL . 'assets/css/gutenberg-jalali-calendar.build' . $suffix . '.css',
 			array( 'wp-edit-blocks' )
 		);
 	}
